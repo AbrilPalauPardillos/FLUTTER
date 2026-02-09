eb7ecfd6555672f06a2431f978a3932b22f70a28
Abril Palau Pardillos <abrilpalau14@gmail.com>
2026-02-09 12:50:47 +0100

    toda la actividad hecha

---
+++ actividad_1/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'pages/homePage.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Nombre interno de la app (visible en tareas recientes en Android).
+      title: 'Mi primera app de FLUTTER',
+
+      // Oculta la cinta "debug" en la esquina superior derecha.
+      debugShowCheckedModeBanner: false,
+
+      // Definición del tema clásico de Material 2 usando primarySwatch.
+      // Esto asigna automáticamente tonos del color azul a la app.
+      theme: ThemeData(primarySwatch: Colors.orange),
+
+      // Pantalla inicial de la aplicación.
+      home: const Homepage(),
+    );
+  }
+}
